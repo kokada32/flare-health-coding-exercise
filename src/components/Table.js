d83fce2a1b4ba03b8a3569578caea7ac66661908
Kenny Okada <kokada32@gmail.com>
2022-02-11 12:09:20 -0800

    created function to map thru each airline array by month to clean repeat code

--- src/components/Table.js
+++ src/components/Table.js
@@ -11,55 +11,14 @@ const Table = props => {
         return Airline.Time.Year === props.selectedYear
     });
 
-    //Filter by Selected Airlines per Month
-    const selectedAirlineJan = newAirlineYearArray.filter( Airline => {
-        return Airline.Time['Month Name'] === 'January' && Airline.Time.Year === props.selectedYear
+    //Filter by Month
+    const monthsArray = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const selectedAirline = monthsArray.map( month => {
+        const n = newAirlineYearArray.filter( Airline => {
+            return Airline.Time['Month Name'] === month
+        });
+        return n
     });
-
-    const selectedAirlineFeb = newAirlineYearArray.filter( Airline => {
-        return Airline.Time['Month Name'] === 'Febuary' && Airline.Time.Year === props.selectedYear
-    });
-
-    const selectedAirlineMar = newAirlineYearArray.filter( Airline => {
-        return Airline.Time['Month Name'] === 'March' && Airline.Time.Year === props.selectedYear
-    });
-
-    const selectedAirlineApr = newAirlineYearArray.filter( Airline => {
-        return Airline.Time['Month Name'] === 'April' && Airline.Time.Year === props.selectedYear
-    });
-
-    const selectedAirlineMay = newAirlineYearArray.filter( Airline => {
-        return Airline.Time['Month Name'] === 'May' && Airline.Time.Year === props.selectedYear
-    });
-
-    const selectedAirlineJun = newAirlineYearArray.filter( Airline => {
-        return Airline.Time['Month Name'] === 'June' && Airline.Time.Year === props.selectedYear
-    });
-
-    const selectedAirlineJul = newAirlineYearArray.filter( Airline => {
-        return Airline.Time['Month Name'] === 'July' && Airline.Time.Year === props.selectedYear
-    });
-
-    const selectedAirlineAug = newAirlineYearArray.filter( Airline => {
-        return Airline.Time['Month Name'] === 'August' && Airline.Time.Year === props.selectedYear
-    });
-
-    const selectedAirlineSep = newAirlineYearArray.filter( Airline => {
-        return Airline.Time['Month Name'] === 'September' && Airline.Time.Year === props.selectedYear
-    });
-
-    const selectedAirlineOct = newAirlineYearArray.filter( Airline => {
-        return Airline.Time['Month Name'] === 'October' && Airline.Time.Year === props.selectedYear
-    });
-
-    const selectedAirlineNov = newAirlineYearArray.filter( Airline => {
-        return Airline.Time['Month Name'] === 'November' && Airline.Time.Year === props.selectedYear
-    });
-
-    const selectedAirlineDec = newAirlineYearArray.filter( Airline => {
-        return Airline.Time['Month Name'] === 'December' && Airline.Time.Year === props.selectedYear
-    });
-
 
     return (
         <div>
@@ -87,62 +46,62 @@ const Table = props => {
                         <td data-label="Name">{props.selectedCode1}</td>
                         <td data-label="Jan">
                             {
-                                props.allAirlineInfo.length > 0 && selectedAirlineJan.length > 0 ? selectedAirlineJan[0].Statistics.Flights.Total : 0 
+                                props.allAirlineInfo.length > 0 && selectedAirline[0].length > 0 ? selectedAirline[0][0].Statistics.Flights.Total : 0 
                             }
                         </td>
                         <td data-label="Feb">
                             {
-                                props.allAirlineInfo.length > 0 && selectedAirlineFeb.length > 0 ? selectedAirlineFeb[0].Statistics.Flights.Total : 0 
+                                props.allAirlineInfo.length > 0 && selectedAirline[1].length > 0 ? selectedAirline[1][0].Statistics.Flights.Total : 0 
                             }
                         </td>
                         <td data-label="Mar">
                             {
-                                props.allAirlineInfo.length > 0 && selectedAirlineMar.length > 0 ? selectedAirlineMar[0].Statistics.Flights.Total : 0 
+                                props.allAirlineInfo.length > 0 && selectedAirline[2].length > 0 ? selectedAirline[2][0].Statistics.Flights.Total : 0 
                             }
                         </td>
                         <td data-label="Apr">
                             {
-                                props.allAirlineInfo.length > 0 && selectedAirlineApr.length > 0 ? selectedAirlineApr[0].Statistics.Flights.Total : 0 
+                                props.allAirlineInfo.length > 0 && selectedAirline[3].length > 0 ? selectedAirline[3][0].Statistics.Flights.Total : 0 
                             }
                         </td>
                         <td data-label="May">
                             {
-                                props.allAirlineInfo.length > 0 && selectedAirlineMay.length > 0 ? selectedAirlineMay[0].Statistics.Flights.Total : 0 
+                                props.allAirlineInfo.length > 0 && selectedAirline[4].length > 0 ? selectedAirline[4][0].Statistics.Flights.Total : 0 
                             }
                         </td>
                         <td data-label="Jun">
                             {
-                                props.allAirlineInfo.length > 0 && selectedAirlineJun.length > 0 ? selectedAirlineJun[0].Statistics.Flights.Total : 0 
+                                props.allAirlineInfo.length > 0 && selectedAirline[5].length > 0 ? selectedAirline[5][0].Statistics.Flights.Total : 0 
                             }
                         </td>
                         <td data-label="Jul">
                             {
-                                props.allAirlineInfo.length > 0 && selectedAirlineJul.length > 0 ? selectedAirlineJul[0].Statistics.Flights.Total : 0
+                                props.allAirlineInfo.length > 0 && selectedAirline[6].length > 0 ? selectedAirline[6][0].Statistics.Flights.Total : 0
                             }
                         </td>
                         <td data-label="Aug">
                             {
-                                props.allAirlineInfo.length > 0 && selectedAirlineAug.length > 0 ? selectedAirlineAug[0].Statistics.Flights.Total : 0
+                                props.allAirlineInfo.length > 0 && selectedAirline[7].length > 0 ? selectedAirline[7][0].Statistics.Flights.Total : 0
                             }
                         </td>
                         <td data-label="Sep">
                             {
-                                props.allAirlineInfo.length > 0 && selectedAirlineSep.length > 0 ? selectedAirlineSep[0].Statistics.Flights.Total : 0
+                                props.allAirlineInfo.length > 0 && selectedAirline[8].length > 0 ? selectedAirline[8][0].Statistics.Flights.Total : 0
                             }
                         </td>
                         <td data-label="Oct">
                             {
-                                props.allAirlineInfo.length > 0 && selectedAirlineOct.length > 0 ? selectedAirlineOct[0].Statistics.Flights.Total : 0 
+                                props.allAirlineInfo.length > 0 && selectedAirline[9].length > 0 ? selectedAirline[9][0].Statistics.Flights.Total : 0 
                             }
                         </td>
                         <td data-label="Nov">
                             {
-                                props.allAirlineInfo.length > 0 && selectedAirlineNov.length > 0 ? selectedAirlineNov[0].Statistics.Flights.Total : 0 
+                                props.allAirlineInfo.length > 0 && selectedAirline[10].length > 0 ? selectedAirline[10][0].Statistics.Flights.Total : 0 
                             }
                         </td>
                         <td data-label="Dec">
                             {
-                                props.allAirlineInfo.length > 0 && selectedAirlineDec.length > 0 ? selectedAirlineDec[0].Statistics.Flights.Total : 0 
+                                props.allAirlineInfo.length > 0 && selectedAirline[11].length > 0 ? selectedAirline[11][0].Statistics.Flights.Total : 0 
                             }
                         </td>
                         <td data-label="Mean"></td>
